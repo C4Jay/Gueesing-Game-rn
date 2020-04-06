2c44839e20ce28785e2e4e2527f017e055d8fd54
C4Jay <choolakejay@gmail.com>
2020-04-06 10:31:01 +0530

    your-num

--- screens/GamebeginScreen.js
+++ screens/GamebeginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback , Keyboard} from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback , Keyboard, Alert} from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
 
@@ -8,6 +8,8 @@ const GamebeginScreen = props => {
 
     const [valueInput, setvalueInput] = useState('')
     const [show, setshow] = useState(false)
+    const [userconfirm, setuserconfirm] = useState(false)
+    const [numPicked, setnumPicked] = useState('')
 
     const inputHandler = (number) => {
         setvalueInput(number.replace(/[^0-9]/g, ''))
@@ -15,17 +17,36 @@ const GamebeginScreen = props => {
 
     const resetHandler = () => {
         setvalueInput('')
+        setuserconfirm(false)
         setshow(false)
     }
 
     const confirmHandler = () => {
+        if(isNaN(parseInt(valueInput)) || parseInt(valueInput) <= 0 || parseInt(valueInput) > 99) {
+            Alert.alert(
+                'Wrong Input',
+                'Must be between 1 - 99',
+                [{text: 'Okay', style: 'destructive', onPress:resetHandler}]
+            )
+            return
+        }
         setshow(true)
+        setuserconfirm(true)
+        setnumPicked(parseInt(valueInput))
+        setvalueInput('')
     }
+
+    let confirmedCheck;
+
+    if(userconfirm){
+        confirmedCheck = <View><Text>{valueInput}</Text></View>
+    }
+
     return (
         <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
         <View style={styles.screen}>
-        <Text style={styles.title}>the Game Screen</Text>
-        
+       {/*  <Text style={styles.title}>the Game Screen</Text>
+        */} 
         <Card style={styles.inputfield}>
            <Text>Pick a number</Text>
            <Input onChangeText={inputHandler} value={valueInput} style={styles.textfield} blurOnSubmit keyboardType="number-pad" maxLength={2} /> 
@@ -34,7 +55,22 @@ const GamebeginScreen = props => {
               <View style={styles.btn} ><Button onPress={confirmHandler} title="confirm" color="#f7287e" ></Button></View>
            </View>
            </Card>
-           {show ? <View style={{marginTop: 10}}><Card><Text style={{color: 'yellowgreen', fontSize: 36}}>{valueInput}</Text></Card></View> : null}           
+         {show ? 
+         <View style={{marginTop: 10}}>
+             <Card style={{alignItems: 'center'}}>
+             <View style={{marginBottom: 15}}>
+             <Text style={{fontSize: 15}}>Your number</Text>
+             </View>
+             <View style={styles.numview}>
+             <Text style={{fontSize: 28}} >{numPicked}</Text>
+             </View>
+             <View style={styles.btn}>
+             <Button color="yellowgreen" title="next"></Button>
+             </View>
+             </Card>
+             </View> 
+             : null}            
+    
         </View>
         </TouchableWithoutFeedback>
     )
@@ -73,11 +109,25 @@ const styles = StyleSheet.create({
         padding: 1
     },
     btn: {
-        width: 100
+        width: 100,
+        marginTop: 8
     },
     textfield: {
         width: 50,
-        textAlign: 'center'
+        textAlign: 'center',
+        fontSize: 28
+    },
+    numview: {
+        width:168,
+        borderWidth: 3,
+        borderColor: '#c717fc',
+        justifyContent: 'center',
+        textAlign: 'center',
+        height: 68,
+        borderRadius: 6,
+        fontSize: 38,
+        alignItems: 'center'
+
     }
 })
 
